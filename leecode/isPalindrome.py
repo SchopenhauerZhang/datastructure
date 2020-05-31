2de6036f14034bcb33d3880f5bdb623a0815544e
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-05-31 23:57:40 +0800

    添加回文串

--- leecode/isPalindrome.py
+++ leecode/isPalindrome.py
@@ -33,3 +33,27 @@ class Solution:
 
         return x==y
 #print(Solution().isPalindrome_better(10))
+    def isPalindrome_nums_str(self, s: str) -> bool:
+        """
+        给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+        说明：本题中，我们将空字符串定义为有效的回文串。
+        """
+        if not s:
+            return True
+        import re
+        strs = s.lower().strip()
+    
+        strs = re.findall('[a-zA-Z0-9]',strs)
+        if strs == strs[::-1]:
+            return True
+        return False
+
+#print(Solution().isPalindrome_nums_str("A man, a plan, #$@!a canal: Panama"))
+    def isPalindrome_nums_str_better(self, s: str) -> bool:
+        alpha_filter = filter(str.isalnum,s)
+        strs = "".join(alpha_filter)
+        strs1=strs[::-1]
+        if strs.upper()==strs1.upper():
+            return True
+        return False
