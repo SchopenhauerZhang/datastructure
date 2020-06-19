eb5dcddf25007f3ad9aa0f7cea4fc4b3b7027714
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-06-19 09:39:57 +0800

    add合并区间

--- leecode/merge.py
+++ leecode/merge.py
@@ -20,7 +20,62 @@ class Solution:
         """
         pass
 
-    def merge(self, intervals: list) -> list:
-        
+    def _merge(self, intervals: list) -> list:
+        if len(intervals)<=1:
+            return intervals
+        interval = sorted(intervals) 
+        #if len(intervals) ==2:
+        #    return [[intervals[0][0],intervals[1][1]]] if intervals[0][1]>= intervals[1][0] else intervals
+        start ,end = intervals[0]
+        res = []
 
+        for i in interval:
+            if min(i) <= max(start,end): 
+                start = min(start,i[0])
+                end = max(end,i[1])
+            else:
+                res.append([start,end])
+                start = i[0]
+                end = i[1]
+        res.append([start,end])
+        return res
+# print(Solution()._merge([[1,4],[0,0]]))
+# print(Solution()._merge([[1,4],[0,4]]))
+# print(Solution()._merge([[1,4],[4,5]]))
+# print(Solution()._merge([[1,3],[2,6],[8,10],[15,18]]))
 
+    def _merge_(self,intervals):
+        if not intervals: return []
+        # 按区间的 start 升序排列
+        intervals.sort(key=lambda intv: intv[0])
+        res = []
+        res.append(intervals[0])
+
+        for i in range(1, len(intervals)):
+            curr = intervals[i]
+            # res 中最后一个元素的引用
+            last = res[-1]
+            if curr[0] <= last[1]:
+                # 找到最大的 end
+                last[1] = max(last[1], curr[1])
+            else:
+                # 处理下一个待合并区间
+                res.append(curr)
+        return res
+
+    def _merge_(self,intervals):
+        if not intervals:
+            return []
+        intervals.sort(key = lambda a:a[0])
+        ans = []
+        begin = intervals[0][0]
+        end = intervals[0][1]
+        for interval in intervals[1:]:
+            if interval[0] > end:
+                ans.append([begin,end])
+                begin = interval[0]
+                end = interval[1]
+            else:
+                end = max(end, interval[1])
+        ans.append([begin, end])
+        return ans
