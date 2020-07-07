bdef8bcdaa250b21418f143c3fd14b21363f46e4
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-07-07 11:34:50 +0800

    镜像二叉树

--- leecode/isBalanced.py
+++ leecode/isBalanced.py
@@ -29,3 +29,13 @@ class Solution:
 
 
         return False if height(root) is False else True
+
+    def _isBalanced_eg(self, root: TreeNode) -> bool:  
+        def dfs(root):
+            if not root: return 0
+            left = dfs(root.left)
+            if left == -1: return -1
+            right= dfs(root.right)
+            if right == -1: return -1
+            return max(left, right) + 1 if abs(left - right) <= 1 else -1
+        return dfs(root) != -1
