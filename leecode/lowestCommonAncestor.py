6c9fc1abd51b0b8d824323fdfd415f58b7c79e21
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-08-05 07:00:20 +0800

    二叉树的最近公共祖先

--- leecode/lowestCommonAncestor.py
+++ leecode/lowestCommonAncestor.py
@@ -92,3 +92,35 @@ class Solution:
             else:
                 root = root.right
         return root
+    
+    def _lowestCommonAncestor_235(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+            给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+
+        百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+        例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+
+        来源：力扣（LeetCode）
+        链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        """
+        if not root:
+            return None
+        while root:
+            if p.val > root.val and q.val > root.val:
+                root = root.right
+            elif p.val < root.val and q.val < root.val:
+                root = root.left
+            else:
+                return root
+    
+    def _lowestCommonAncestor_235_eg(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        if root and root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        if root and root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        
+        
+        return root
