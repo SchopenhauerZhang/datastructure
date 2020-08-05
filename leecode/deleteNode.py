f320fb26f903ef120255631516b2ca6ad331eb23
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-08-06 07:09:06 +0800

    二叉树判断

--- leecode/deleteNode.py
+++ leecode/deleteNode.py
@@ -86,3 +86,33 @@ class Solution:
         
         return root
         
+    def _deleteNode_237(self, node):
+        """
+        请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点 。
+
+ 
+
+        现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+
+        来源：力扣（LeetCode）
+        链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list
+        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        输入：head = [4,5,1,9], node = 5
+输出：[4,1,9]
+解释：给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        def deln(h,n):
+            if not h:
+                return None
+            if h.val == n:
+                return h.next
+            h.next = deln(h.next,n)
+            return h
+        del(head,node)
+            
--- leecode/isValidBST.py
+++ leecode/isValidBST.py
@@ -60,3 +60,50 @@ class Solution:
                 res.append(node.val)
                 node = node.right
         return True
+    
+    def _isValidBST_0405(self, root: TreeNode,left=float('-inf',right=float('inf'))) -> bool:
+        """
+                实现一个函数，检查一棵二叉树是否为二叉搜索树。
+
+                示例 1:
+                输入:
+                    2
+                / \
+                1   3
+                输出: true
+                示例 2:
+                输入:
+                    5
+                / \
+                1   4
+                     / \
+                    3   6
+                输出: false
+                解释: 输入为: [5,1,4,null,null,3,6]。
+                     根节点的值为 5 ，但是其右子节点值为 4 。
+
+                来源：力扣（LeetCode）
+                链接：https://leetcode-cn.com/problems/legal-binary-search-tree-lcci
+                著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        """
+        if not root:
+            return True
+        if root.val <=left or root.val >=right:
+            return False
+        else:
+            return self._isValidBST_0405(root.left,left,min(right,root.val)) and self._isValidBST_0405(root.right,max(root.val,left),right)
+    def __init__(self):
+        super().__init__()
+        self.pre = None
+    def _isValidBST_0405_eg(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        if not self.isValidBST(root.left):
+            return False
+        if self.pre and root.val <= self.pre.val:
+            return False
+        self.pre = root
+        if not self.isValidBST(root.right):
+            return False
+
+        return True
