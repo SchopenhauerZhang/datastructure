00c2256dcabdd6209a9e51389c9fa21f187bd68f
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-08-07 07:19:15 +0800

    位1的个数

---
+++ leecode/hammingWeight.py
@@ -0,0 +1,32 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        """
+            编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。
+
+ 
+
+            示例 1：
+
+            输入：00000000000000000000000000001011
+            输出：3
+            解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
+            示例 2：
+
+            输入：00000000000000000000000010000000
+            输出：1
+            解释：输入的二进制串 00000000000000000000000010000000 中，共有一位为 '1'。
+
+            来源：力扣（LeetCode）
+            链接：https://leetcode-cn.com/problems/number-of-1-bits
+            著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        """
+        if not n or n == 0 or n ==1:
+            return 0 if n != 1 else 1
+        return bin(n & 0xffffffff).count('1')
+    
+    def _hammingWeight_eg(self, n: int) -> int:
+        count = 0
+        while n:
+            count += (n & 1)
+            n = n >> 1
+        return count
