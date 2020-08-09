76958da1f96a997f2804b9b27bdb441ca444cb11
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-08-09 19:41:44 +0800

    add 最小k个数,n个数的和

---
+++ leecode/sumNums.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sumNums(self, n: int) -> int:
+        """
+            求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+             
+
+            示例 1：
+
+            输入: n = 3
+            输出: 6
+            示例 2：
+
+            输入: n = 9
+            输出: 45
+
+            来源：力扣（LeetCode）
+            链接：https://leetcode-cn.com/problems/qiu-12n-lcof
+            著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        """
+        if n==1:
+            return 1
+        return n+self.sumNums(n-1)
+    
+    def sumNums_eg(self, n: int) -> int:
+        return sum(range(1,n+1))
