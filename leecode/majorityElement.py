af6c56c94016ce5abac35e3454160791aba5a34f
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-08-13 06:56:17 +0800

    add 出现次数粗超过一半的元素

--- leecode/majorityElement.py
+++ leecode/majorityElement.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def majorityElement(self, nums: list) -> int:
         """
@@ -35,5 +36,36 @@ class Solution:
         a = int(len(nums)/2)
         return nums[a]
 
-        
+    def _majorityElement_39(self, nums: List[int]) -> int:
+        """
+            数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+
+             
+
+            你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+             
+
+            示例 1:
+
+            输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+            输出: 2
+             
+
+            限制：
+
+            1 <= 数组长度 <= 50000
+
+             
+
+            来源：力扣（LeetCode）
+            链接：https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof
+            著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        """
+        if len(nums) <=2:
+            return nums[-1] 
+        return sorted(nums)[len(nums)//2]
+
+    
+
 print(Solution()._majorityElement_eg([2,2,1,1,1,2,2]))
