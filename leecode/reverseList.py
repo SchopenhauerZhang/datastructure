c1cab426557b36edc77fbc9ae4396d91e556659d
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-08-12 08:57:27 +0800

    add 反转链表

--- leecode/reverseList.py
+++ leecode/reverseList.py
@@ -3,7 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+from typing import List
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         """
@@ -46,5 +46,66 @@ class Solution:
         head.next = None
         return rev
 
+    def _reverseList_24(self, head: ListNode) -> ListNode:
+        """
+                定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+            示例:
+
+            输入: 1->2->3->4->5->NULL
+            输出: 5->4->3->2->1->NULL
+
+            来源：力扣（LeetCode）
+            链接：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof
+            著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        """
+        if not head:
+            return head
+        pre,cur,_next = None,head,head.next
+
+        while _next:
+            cur.next,cur,pre,_next = pre,_next,cur,_next.next
+        cur.next = pre
+
+        return cur
+
+    def _reverseList_24_eg(self, head: ListNode) -> ListNode:
+    
+        # 方法1：构建链表的副本 时间复杂度：o(n),空间复杂度：o(n)
+        # 双指针：一个指针用作反转后链表的头节点，另一个指针用于遍历原链表
+        # if not head:
+        #     return None
+        # travel = head # 用于遍历原链表
+        # reHead = None # 反转后 链表的头节点
+        # while travel:
+        #     # 这个临时节点就相当于一个副本
+        #     temp = ListNode(travel.val)
+        #     temp.next = reHead
+        #     reHead = temp
+        #     travel = travel.next
+        # return reHead
+    
+        # 方法2：三指针：后两个指针用于交换顺序，最前的用于确定遍历位置
+        # 特殊情况：链表为空 or 只有一个节点
+        if not head or not head.next:
+            return head
+        # # 特殊情况：只有两个节点
+        # if not head.next.next:
+        #     A = head
+        #     B = head.next
+        #     A.next = None
+        #     B.next = A
+        #     return B
+        # 至少3个节点时
+        A = head
+        B = head.next
+        C = head.next.next
+        A.next = None    
+        while C:
+            B.next = A
+            A = B
+            B = C
+            C = C.next
+        B.next = A
+        return B
 
 
