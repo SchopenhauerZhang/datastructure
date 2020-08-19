e44a9e58bc35df9f305d12d24aa0998e78660d9c
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-08-19 23:16:51 +0800

    组合

--- leecode/combinationSum.py
+++ leecode/combinationSum.py
@@ -1,19 +1,72 @@
 from typing import List
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        if len(candidates) <= 1 or not  target:
-            return [[candidates[0]]*(target//candidates[0])] if candidates and target%candidates[0]==0 else []
-        res = []
-        def combination(can,tagt,_res):
-            nonlocal res
-            for i in range(len(can)):
-                if tagt % can[i] == 0:
-                    res.append(_res+[can[i]*(tagt // can[i])])
-                else:
-                    _tagt = _tagt % can[i]
-                    _res = _res+[can[i]*(_tagt // can[i])]
-                    combination(can[i+1:],_tagt,_res)
-            
-        combination(candidates,target,[])
+        """
+            给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
 
-        return res
+            candidates 中的数字可以无限制重复被选取。
+
+            说明：
+
+            所有数字（包括 target）都是正整数。
+            解集不能包含重复的组合。 
+            示例 1：
+
+            输入：candidates = [2,3,6,7], target = 7,
+            所求解集为：
+            [
+            [7],
+            [2,2,3]
+            ]
+            示例 2：
+
+            输入：candidates = [2,3,5], target = 8,
+            所求解集为：
+            [
+              [2,2,2,2],
+              [2,3,3],
+              [3,5]
+            ]
+             
+
+            提示：
+
+            1 <= candidates.length <= 30
+            1 <= candidates[i] <= 200
+            candidate 中的每个元素都是独一无二的。
+            1 <= target <= 500
+
+            来源：力扣（LeetCode）
+            链接：https://leetcode-cn.com/problems/combination-sum
+            著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        """
+        if not candidates or not target:
+            return []
+        candidate = sorted(candidates)
+        self.res = []
+        def combin(nums,tgt,sub,_max):
+            if not nums:
+                return 
+            if tgt == 0:
+                self.res.append(sub)
+            if tgt < nums[0]:
+                return 
+            
+            for num in nums:
+                if num > tgt:
+                    return 
+                if num < _max:
+                    continue
+                
+                combin(nums,tgt-num,sub+[num],num)
+        combin(candidate,target,[],0)
+        return self.res
+
+    def _combinationSum_eg(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp = [[] for _ in range(target + 1)]
+        dp[0] = [[]]
+        for item in candidates:
+            for tar in range(item,target + 1):
+                for i in dp[tar - item]:
+                    dp[tar].append(i + [item])
+        return dp[target]
--- leecode/maxCoins.py
+++ leecode/maxCoins.py
@@ -17,13 +17,44 @@ class Solution:
         输出: 167 
         解释: nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
             coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
+        没看懂:https://blog.csdn.net/u014626513/article/details/81146851
         """
-        pass
-    
-    def _maxCoins(self, nums: list) -> int:
-        
+        # 从中间开始戳气球
+        coins = [1] + [i for i in nums if i > 0] + [1]
+        n = len(coins)
+        max_coins = [[0 for _ in range(n)] for _ in range(n)]
+        [1,3,1,5,8,1]
+        for k in range(2, n):
+            for left in range(n - k):
+                right = left + k
+                for i in range(left + 1, right):
+                    max_coins[left][right] = max(max_coins[left][right], \
+                           coins[left] * coins[i] * coins[right] + \
+                           max_coins[left][i] + max_coins[i][right])
+        print(max_coins)
+        return max_coins[0][-1]
 
+    def _maxCoins_eg(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
 
-        return 0
+        nums = [1] + nums + [1]
+        n = len(nums)
 
-print(Solution()._maxCoins([3,1,5,8]))
+        dp = [[0] * n for _ in range(n)]
+
+        for i in reversed(range(n-2)):
+            for j in range(i + 2, n):
+                max = 0
+                product = nums[i] * nums[j]
+                for k in range(i + 1, j):
+                    tmp = dp[i][k] + dp[k][j] + nums[k] * product
+                    if tmp > max:
+                        max = tmp
+                
+                dp[i][j] = max
+                
+        return dp[0][n-1]
+
+#print(Solution().maxCoins([3,1,5,8]))
+print(Solution().maxCoins([3,1,2,5,4,8]))# 348
