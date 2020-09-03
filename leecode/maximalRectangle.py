f5116c81cf74494ed7fcd5bed9dcd5355ec3385c
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-09-04 00:58:05 +0800

    添加子矩阵的解体思路

--- leecode/maximalRectangle.py
+++ leecode/maximalRectangle.py
@@ -56,13 +56,72 @@ class Solution:
         for i in range(N):
             j, num = i, nums[i]
             while j < N:
+                ### 这里是为了找到临近的2行相同的1的个数，j就相当于是宽
+                ### 【1，1，1，0】
+                ### 【0，1，1，0】
+                ### &结果为【0，1，1，0】
                 num = num&nums[j]
                 if not num:
                     break
                 width, curnum = 0, num
+                # 找到结果中1的个数（相当于长）
                 while curnum:
                     width += 1
                     curnum = curnum&(curnum>>1)
                 ans = max(ans, width*(j-i+1))
                 j+=1
         return ans 
+
+    def _maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        # 每行0，1的二进制转为整数，或者直接二进制bin保存也可以
+        int_dp = [int(''.join(i),base=2) for i in matrix]
+        ll = len(int_dp)
+        res = 0
+        for i in range(ll):
+            int_num = int_dp[i]
+            iterm = i
+            while iterm<ll:
+                # 逐个&,求长
+                int_num = int_num&int_dp[iterm]
+                if not int_num:
+                    break
+                width = 0
+                num = int_num
+                while num:
+                    # 求宽,既是num中1的个数
+                    width += 1
+                    num = num&(num>>1)
+                    # 长*宽
+                    res = max(res,width*(iterm-i+1))
+
+                iterm += 1
+
+        return res
+
+    def _maximalRectangle_submastrix(self, matrix: List[List[str]]) -> int:
+        #子矩阵解法
+        dp = [[0]*(len(matrix[0])+1)]*(len(matrix)+1)
+        m,n = len(matrix)+1,len(matrix[0])+1 
+        # dp[m][n]为矩阵m*n的最大和
+        dp[1][0] = 1 if matrix[1][0] == '1' else 0
+        dp[0][1] = 1 if matrix[0][1] == '1' else 0
+        dp[0][0] = 1 if matrix[0][0] == '1' else 0
+        l,r = 1,1
+        res = 0
+        while l < m and r < n:
+        
+            # 求出子矩阵中最大的矩形
+            dp[l][r] = dp[l-1][r]+dp[l][r-1]-dp[l-1][r-1]+(1 if matrix[l][r]=='1' else 0)
+            # 如下的矩阵就算不出来
+            # 1，1，1，1，
+            # 1，1，1，1，
+            # 1，0，1，1，
+            # 1，1，1，1，
+            # 1，1，1，1，
+
+            res = max(res,dp[l][r])
+            
+        return 
+
