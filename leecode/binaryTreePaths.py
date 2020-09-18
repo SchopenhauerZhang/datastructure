b0f24de49c46f21e5c2d6499dc304a3fbbeefe8f
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-09-18 09:35:54 +0800

    add 二叉树的所有路径

--- leecode/binaryTreePaths.py
+++ leecode/binaryTreePaths.py
@@ -6,40 +6,68 @@
 #         self.right = None
 from typing import List
 class Solution:
-    self.l = []
     def binaryTreePaths(self, root: TreeNode) -> List[str]:
-        def treepath(r,s):
-            if not r:
-                return s
-            s = s + '->'+str(r.val)
-            sl = s + self.binaryTreePaths(r.left)
-            if sl:
-                self.l.append(sl)
-            sr = s + self.binaryTreePaths(r.right)
-            if sr:
-                self.l.append(sr)
-            return s
-        s = str(root.val)
-        treepath(root.left,s)
-        treepath(root.right,s)
-        return self.l
-    
-    def _binaryTreePaths_257(self, root: TreeNode) -> List[str]:
+        """
+            给定一个二叉树，返回所有从根节点到叶子节点的路径。
+
+            说明: 叶子节点是指没有子节点的节点。
+
+            示例:
+
+            输入:
+
+            1
+            /   \
+            2     3
+            \
+            5
+
+            输出: ["1->2->5", "1->3"]
+
+            解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+
+            来源：力扣（LeetCode）
+            链接：https://leetcode-cn.com/problems/binary-tree-paths
+            著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        """
         if not root or (not root.left and not root.right):
             return [] if not root else [''+str(root.val)+'']
         def tree(node,_res):
             if not node:
                 return None
-            _resl = tree(node.left,_res+'->'+str(node.val))
-            _resr = tree(node.right,_res+'->'+str(node.val))
-            if not _resl:
-                res.append(_resl)
-            if not _resr:
-                res.append(_resr)
-            if not _resl or not _resr:
-                return _res
-            return _resl if _resl else _resr
+      
+            _res = str(node.val) if not _res else _res+'->'+str(node.val)
+            if node.left:
+                tree(node.left,_res)
+            if node.right :
+                tree(node.right,_res)
+            if not node.left and not node.right:
+                if not _res:
+                    return 
+                print(res)
+                res.append(_res)
+            return 
         
         res = []
-        res.append(tree(root,''))
-        return res 
+        tree(root,'')
+        return res
+
+    def binaryTreePaths_eg(self, root: TreeNode) -> List[str]:
+        if not root:
+            return []
+        res=[]
+        def dfs(root,path):
+            path+=str(root.val)
+            if not root.left and not root.right:
+                res.append(path)
+            elif not root.left:
+                dfs(root.right,path+'->')
+            elif not root.right:
+                dfs(root.left,path+'->')
+            else:
+                dfs(root.right,path+'->')
+                dfs(root.left,path+'->')
+
+
+        dfs(root,'')
+        return res
