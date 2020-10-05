80ab063a484f1ae41c82073a45e7ab3f9a6f1287
schopenhauerzhang <schopenhauerzhang@gmail.com>
2020-10-05 15:03:43 +0800

    求出统计全为 1 的正方形子矩阵

--- leecode/numTrees.py
+++ leecode/numTrees.py
@@ -19,6 +19,22 @@ class Solution:
             来源：力扣（LeetCode）
             链接：https://leetcode-cn.com/problems/unique-binary-search-trees
             著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+            假设n个节点存在
+
+令G(n)的从1到n可以形成二叉排序树个数
+
+令f(i)为以i为根的二叉搜索树的个数
+
+即有:G(n) = f(1) + f(2) + f(3) + f(4) + ... + f(n)
+
+n为根节点，当i为根节点时，其左子树节点个数为[1,2,3,...,i-1]，右子树节点个数为[i+1,i+2,...n]，所以当i为根节点时，其左子树节点个数为i-1个，右子树节点为n-i，即f(i) = G(i-1)*G(n-i),
+
+上面两式可得:G(n) = G(0)*G(n-1)+G(1)*(n-2)+...+G(n-1)*G(0)
+
+作者：powcai
+链接：https://leetcode-cn.com/problems/unique-binary-search-trees/solution/dong-tai-gui-hua-by-powcai-4/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
         """
         if not n or n<=2:
             return 0 if not n else n
